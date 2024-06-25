680f3f9bded576b8b9dd02880c303384d36ce61e
StevenDelval <asteven59220@free.fr>
2024-06-25 10:39:32 +0200

    change start_urls

--- moviescraper/moviescraper/spiders/allocine.py
+++ moviescraper/moviescraper/spiders/allocine.py
@@ -4,7 +4,7 @@ import scrapy
 class AllocineSpider(scrapy.Spider):
     name = "allocine"
     allowed_domains = ["allocine.fr"]
-    start_urls = ["https://allocine.fr"]
+    start_urls = ["https://allocine.fr/film/meilleurs/"]
 
     def parse(self, response):
         pass
