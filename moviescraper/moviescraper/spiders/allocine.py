09a64c82d0a4d7c0a570e864c3f254b4ad53c985
StevenDelval <asteven59220@free.fr>
2024-06-25 11:57:04 +0200

    setup link extractor and start requests

--- moviescraper/moviescraper/spiders/allocine.py
+++ moviescraper/moviescraper/spiders/allocine.py
@@ -1,10 +1,26 @@
 import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from ..items import MoviescraperItem
 
-
-class AllocineSpider(scrapy.Spider):
+class AllocineSpider(CrawlSpider):
     name = "allocine"
     allowed_domains = ["allocine.fr"]
-    start_urls = ["https://allocine.fr/film/meilleurs/"]
+    start_urls = [f"https://www.allocine.fr/film/meilleurs/?page={page}" for page in range(1, 2)]
+    
 
-    def parse(self, response):
-        pass
+    link_allo_details = LinkExtractor(restrict_xpaths="//h2/a")
+    
+    rule_allo_details= Rule(link_allo_details, callback='parse_item', follow=True)
+    
+    rules = (
+        rule_allo_details,
+    )
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url=url)
+
+    def parse_item(self, response):
+        item = MoviescraperItem()
+        print("ok")
