9b0eed538ee5d44962af7bd9842775755b398bd6
StevenDelval <asteven59220@free.fr>
2024-06-27 16:49:52 +0200

    Add xpath

--- moviescraper/moviescraper/spiders/allocine.py
+++ moviescraper/moviescraper/spiders/allocine.py
@@ -30,25 +30,26 @@ class AllocineSpider(CrawlSpider):
         next_page = self.link_next_page.extract_links(response)
         for link in links:
             yield scrapy.Request(link.url, self.parse_item, meta={"playwright": True})
+        
 
         # Follow next page link
-        if next_page:
-            yield scrapy.Request(next_page[0].url, self.parse_start_url, meta={"playwright": True})        
+        # if next_page:
+        #     yield scrapy.Request(next_page[0].url, self.parse_start_url, meta={"playwright": True})        
 
     def parse_item(self, response):
         item = MoviescraperItem()
         item["titre"] = ''.join(response.xpath("//div[@class='titlebar-title titlebar-title-xl']/text()").extract())
-        item["titre_original"] = ""
-        item["score"] = ""
-        item["genre"] = ""
-        item["date"] = ""
-        item["duree"] = ""
-        item["descriptions"] = ""
-        item["acteurs"] = ""
-        item["realisateur"] = ""
-        item["public"] = ""
-        item["pays"] = ""
-        item["url_image"] = ""
-        item["langue"] = ""
+        item["titre_original"] = ''.join(response.xpath("//div[@class='meta-body-item']/span[contains(text(), 'Titre original')]/following-sibling::span[1]/text()").extract())
+        item["score"] = ''.join(response.xpath().extract())
+        item["genre"] = ''.join(response.xpath().extract())
+        item["date"] = ''.join(response.xpath("//a[@class='xXx date blue-link']/text()").extract())
+        item["duree"] = ''.join(response.xpath("//span[@class='spacer'][1]/following-sibling::text()[1]").extract())
+        item["descriptions"] = ''.join(response.xpath().extract())
+        item["acteurs"] = ''.join(response.xpath().extract())
+        item["realisateur"] = ''.join(response.xpath().extract())
+        item["public"] = ''.join(response.xpath("//div[@class='certificate']/span[@class='certificate-text']/text()").extract())
+        item["pays"] = ''.join(response.xpath("//a[@class='xXx nationality']/text()").extract())
+        item["url_image"] = ''.join(response.xpath().extract())
+        item["langue"] = ''.join(response.xpath("//div[@class='item']/span[contains(text(), 'Langues')]/following-sibling::span[1]/text()").extract())
         
         yield item
